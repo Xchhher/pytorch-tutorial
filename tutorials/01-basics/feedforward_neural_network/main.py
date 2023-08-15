30acde8b6066531ecfedaad7d0d9b6c27922796e
Xcher <xch_0221@163.com>
2023-08-15 22:13:13 +0800

    测试1

--- tutorials/01-basics/feedforward_neural_network/main.py
+++ tutorials/01-basics/feedforward_neural_network/main.py
@@ -2,7 +2,7 @@ import torch
 import torch.nn as nn
 import torchvision
 import torchvision.transforms as transforms
-
+import random
 
 # Device configuration
 device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
@@ -91,4 +91,4 @@ with torch.no_grad():
     print('Accuracy of the network on the 10000 test images: {} %'.format(100 * correct / total))
 
 # Save the model checkpoint
-torch.save(model.state_dict(), 'model.ckpt')
+torch.save(model.state_dict(), 'mnist_model.ckpt')
